44064176d5d4b415daf519c5eb053d44043ff235
Ricardo Vidal <ricardoqvidal@gmail.com>
2023-04-09 22:02:15 -0300

    Iniciando o projeto Controle de Tarefas

---
+++ app/src/main/java/com/ricardo/controledetarefas/AdicionarTarefas.java
@@ -0,0 +1,14 @@
+package com.ricardo.controledetarefas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdicionarTarefas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adicionar_tarefas);
+    }
+}
